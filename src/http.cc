9b88084c994aa9d1d75baaadeab1caaf59975fa6
William Wilson <zero@ou.edu>
2023-10-05 16:14:39 -0500

    fix SslVerifyHost (0L) flag required for insecure SSL connections

--- src/http.cc
+++ src/http.cc
@@ -190,11 +190,13 @@ minio::http::Response minio::http::Request::execute() {
   if (debug) request.setOpt(new curlpp::Options::Verbose(true));
   if (ignore_cert_check) {
     request.setOpt(new curlpp::Options::SslVerifyPeer(false));
+    request.setOpt(new curlpp::Options::SslVerifyHost(0L));
   }
 
   if (url.https) {
     if (!ssl_cert_file.empty()) {
       request.setOpt(new curlpp::Options::SslVerifyPeer(true));
+      request.setOpt(new curlpp::Options::SslVerifyHost(2L));
       request.setOpt(new curlpp::Options::CaInfo(ssl_cert_file));
     }
     if (!key_file.empty()) {
