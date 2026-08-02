0bc22b5d2261dfd179d3e3c5ec371ff86528c2e3
Eugene Lozko <lozkoeugeniy@gmail.com>
2026-08-03 01:15:41 +0300

    fix(module): use clang-format (Google style)

--- modules/miniocpp.cc
+++ modules/miniocpp.cc
@@ -11,16 +11,16 @@ export module miniocpp;
 // namespace. The list below covers what a typical consumer needs today -
 // extend it by adding more using-declarations as needed.
 export namespace minio::s3 {
-    using s3::BaseUrl;
-    using s3::Client;
-    using s3::BucketExistsArgs;
-    using s3::BucketExistsResponse;
-}
+using s3::BaseUrl;
+using s3::BucketExistsArgs;
+using s3::BucketExistsResponse;
+using s3::Client;
+}  // namespace minio::s3
 
 export namespace minio::creds {
-    using minio::creds::StaticProvider;
+using minio::creds::StaticProvider;
 }
 
 export namespace minio {
-    using minio::Result;
+using minio::Result;
 }
